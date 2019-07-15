da5e6ddf3df7d16a166874bf7be4733c918bb068
Ben Hall <benji2357@gmail.com>
2019-07-15 19:39:07 +0100

    chore(amplify): add auth and rename some things

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,20 @@
+import Amplify, { Auth } from 'aws-amplify'
+import AWSAppSyncClient, { AUTH_TYPE } from 'aws-appsync'
 import { Paper, PaperGroup } from 'eri'
 import React from 'react'
+import { ApolloProvider } from 'react-apollo'
 import awsconfig from '../aws-exports'
 import JobList from './JobList'
-import { ApolloProvider } from 'react-apollo'
-import AWSAppSyncClient from 'aws-appsync'
 import CreateJob from './CreateJob'
+
+// TODO
+Amplify.configure(awsconfig)
 
 const client = new AWSAppSyncClient({
   auth: {
-    apiKey: awsconfig.aws_appsync_apiKey,
-    type: awsconfig.aws_appsync_authenticationType as any,
+    type: AUTH_TYPE.AMAZON_COGNITO_USER_POOLS,
+    jwtToken: async () =>
+      (await Auth.currentSession()).getIdToken().getJwtToken(),
   },
   region: awsconfig.aws_appsync_region,
   url: awsconfig.aws_appsync_graphqlEndpoint,
