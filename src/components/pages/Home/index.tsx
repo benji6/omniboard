6a0d58ed03e32c2b1d1b64f4e285e6fea2862ae2
Ben Hall <benji2357@gmail.com>
2019-07-25 22:32:30 +0100

    feat(filters): add toggle for searching for remote options

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -1,5 +1,5 @@
-import { RouteComponentProps, NavigateFn } from '@reach/router'
-import { Spinner, PaperGroup, Paper } from 'eri'
+import { RouteComponentProps, NavigateFn, WindowLocation } from '@reach/router'
+import { Spinner, PaperGroup, Paper, Toggle } from 'eri'
 import gql from 'graphql-tag'
 import React from 'react'
 import { Query } from 'react-apollo'
@@ -12,17 +12,17 @@ import {
 import JobListItem from './JobListItem'
 
 export default function Home({ location, navigate }: RouteComponentProps) {
-  let filter: ModelJobFilterInput = {}
+  const [filtersApplied, setFiltersApplied] = React.useState(false)
+  const [remoteFilter, setRemoteFilter] = React.useState(false)
 
-  if (location) {
-    const searchParams = new URLSearchParams(location.search)
-    const remoteValue = searchParams.get('remote')
-    if (remoteValue) {
-      try {
-        const remote = JSON.parse(remoteValue)
-        if (typeof remote === 'boolean') filter.remote = { eq: remote }
-      } catch {}
-    }
+  let filter: ModelJobFilterInput = {}
+  const searchParams = new URLSearchParams((location as WindowLocation).search)
+  const remoteValue = searchParams.get('remote')
+  if (remoteValue) {
+    try {
+      const remote = JSON.parse(remoteValue)
+      if (typeof remote === 'boolean') filter.remote = { eq: remote }
+    } catch {}
   }
 
   const variables = Object.keys(filter).length ? { filter } : undefined
@@ -31,6 +31,29 @@ export default function Home({ location, navigate }: RouteComponentProps) {
     <PaperGroup>
       <Paper>
         <h2>Job list</h2>
+        <Toggle
+          checked={filtersApplied}
+          label="Toggle filters"
+          onChange={() => setFiltersApplied(!filtersApplied)}
+        />
+        {filtersApplied && (
+          <Toggle
+            checked={remoteFilter}
+            label="Remote"
+            onChange={() => {
+              const newRemoteFilter = !remoteFilter
+              if (newRemoteFilter) searchParams.set('remote', 'true')
+              else searchParams.delete('remote')
+              ;(navigate as NavigateFn)(
+                `/${[...searchParams].length ? '?' + searchParams : ''}`,
+                {
+                  replace: true,
+                },
+              )
+              setRemoteFilter(newRemoteFilter)
+            }}
+          />
+        )}
       </Paper>
       <Query<ListJobsQuery, ListJobsQueryVariables>
         query={gql(listJobs)}
