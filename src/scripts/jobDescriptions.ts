6c9f2138d4aa79a030f38cf860a2987f23ff4d45
Ben Hall <benji2357@gmail.com>
2019-07-25 20:27:27 +0100

    chore(scripts): add rudimentary code for generating test data

---
+++ src/scripts/jobDescriptions.ts
@@ -0,0 +1,185 @@
+export default [
+  'Must be able to write own job description',
+  `Account Development Manager Job Description Sample
+  Account Development Manager Job Description Sample
+  This account development manager sample job description can assist in your creating a job application that will attract job candidates who are qualified for the job. Feel free to revise this job description to meet your specific job duties and job requirements.
+
+  Account Development Manager Job Responsibilities:
+  Develops new business by analyzing account potential; initiating, developing, and closing sales; recommending new applications and sales strategies.
+
+
+
+  Account Development Manager Job Duties:
+  Identifies development potential in accounts by studying current business; interviewing key customer personnel and company personnel who have worked with customer; identifying and evaluating additional needs; analyzing opportunities.
+  Initiates sales process by building relationships; qualifying potential; scheduling appointments.
+  Develops sales by making initial presentation; explaining product and service enhancements and additions; introducing new products and services.
+  Develops new applications by preparing specifications; conferring with product engineering.
+  Closes sales by overcoming objections; preparing contracts.
+  Contributes information to sales strategies by evaluating current product results; identifying needs to be filled; monitoring competitive products; analyzing and relaying customer reactions.
+  Updates job knowledge by participating in educational opportunities; reading professional publications; maintaining personal networks; participating in professional organizations.
+  Enhances department and organization reputation by accepting ownership for accomplishing new and different requests; exploring opportunities to add value to job accomplishments.
+  Account Development Manager Skills and Qualifications:
+  ClientBase, Establishes Partnerships/Alliances, Prospecting Skills, Meeting Sales Goals, Foster Teamwork, Planning, Building Relationships, People Skills, Initiative, Customer Focus, Emphasizing Excellence`,
+  `responsibilities include:
+  Documenting, monitoring and updating accounting policies
+  Ensuring all payments are completed on time
+  Reviewing quarterly and annual budgets for all departments
+  Job brief
+  We are looking for a Controller to develop accounting policies and oversee all financial procedures of our company.
+
+  What does a Controller do?
+  As a Controller, you’ll monitor the company’s financial status, review quarterly and annual budgets and manage accounting transactions. To be successful in this role, you should have in-depth knowledge of Generally Accepted Accounting Principles (GAAP) and experience with risk management.
+
+  Ultimately, you’ll make sure we comply with accounting regulations, maximize profits and mitigate risks.
+
+  Responsibilities
+  Document, monitor and update accounting policies
+  Ensure all payments are completed on time (including accounts payable and bank deposits)
+  Review quarterly and annual budgets for all departments
+  Analyze costs and profits
+  Set and track financial KPIs
+  Build risk assessment models to evaluate investments
+  Report on financial status on a regular basis and create ad-hoc reports when needed
+  Maintain accurate and up-to-date filing systems
+  Perform internal audits and assist with external audits
+  Ensure compliance with accounting and tax regulations
+  Requirements
+  Work experience as a Controller or similar role
+  Thorough knowledge of accounting principles, procedures and legislation
+  Hands-on experience with general ledger accounts payable and accounts receivable functions
+  Experience with cash management and risk assessment
+  Good knowledge of accounting software
+  Attention to detail
+  Ability to work with tight deadlines
+  BSc or MSc in Accounting or Finance
+  CPA or CMA preferred`,
+  `Accountant General Job Responsibilities:
+  Provides financial information to management by researching and analyzing accounting data; preparing reports.
+
+
+
+  Accountant General Job Duties:
+  Prepares asset, liability, and capital account entries by compiling and analyzing account information.
+  Documents financial transactions by entering account information.
+  Recommends financial actions by analyzing accounting options.
+  Summarizes current financial status by collecting information; preparing balance sheet, profit and loss statement, and other reports.
+  Substantiates financial transactions by auditing documents.
+  Maintains accounting controls by preparing and recommending policies and procedures.
+  Guides accounting clerical staff by coordinating activities and answering questions.
+  Reconciles financial discrepancies by collecting and analyzing account information.
+  Secures financial information by completing data base backups.
+  Maintains financial security by following internal controls.
+  Prepares payments by verifying documentation, and requesting disbursements.
+  Answers accounting procedure questions by researching and interpreting accounting policy and regulations.
+  Complies with federal, state, and local financial legal requirements by studying existing and new legislation, enforcing adherence to requirements, and advising management on needed actions.
+  Prepares special financial reports by collecting, analyzing, and summarizing account information and trends.
+  Maintains customer confidence and protects operations by keeping financial information confidential.
+  Maintains professional and technical knowledge by attending educational workshops; reviewing professional publications; establishing personal networks; participating in professional societies.
+  Accomplishes the result by performing the duty.
+  Contributes to team effort by accomplishing related results as needed.
+  Accountant General Skills and Qualifications:
+  Accounting, Corporate Finance, Reporting Skills, Attention to Detail, Deadline-Oriented, Reporting Research Results, SFAS Rules, Confidentiality, Time Management, Data Entry Management, General Math Skills`,
+  `Dispatcher Job Description
+  Dispatcher Job Description
+  This dispatcher sample job description can assist in your creating a job application that will attract job candidates who are qualified for the job. Feel free to revise this job description to meet your specific job duties and job requirements.
+
+  Dispatcher Job Responsibilities:
+  Delivers items to customers by verifying orders; inventorying stock; arranging transportation.
+
+
+  Dispatcher Job Duties:
+  Organizes item orders by editing for price, promotions, weight compliance.
+  Arranges shipments by checking stock to determine inventory levels; anticipating delivery requirements; placing and expediting orders.
+  Ships items by examining items, destination, route, rate, delivery time; ordering carriers.
+  Verifies items shipped by matching bills of lading; reconciling quantities; noting discrepancies.
+  Keeps customers informed by forwarding notice of item availability, shipment date and method, and current status; answering questions.
+  Replaces damaged items, shortages, and misshipments by determining optimal response of replacement or credit.
+  Protects organization reputation by keeping pricing, promotion, purchase order, and credit-limit information confidential.
+  Updates job knowledge by participating in educational opportunities.
+  Accomplishes organization goals by accepting ownership for accomplishing new and different requests; exploring opportunities to add value to job accomplishments.
+  Dispatcher Skills and Qualifications:
+  Documentation Skills, Data Entry Skills, Telephone Skills, Informing Others, Listening, Verbal Communication, Energy Level, Multi-tasking, Dependability, Emotional Control, Reporting Skills`,
+  `Nurse Consultant Job Responsibilities:
+  Provides healthcare consultations and technical advice by investigating malpractice claims; identifying meritorious cases; avoiding non-rewarding litigation; locating reliable, authoritative experts; preparing conclusive cases.
+
+
+  Nurse Consultant Job Duties:
+  Supports healthcare providers, organizations, and patients by completing investigations for attorneys, physicians, insurance and accreditation organizations, health professionals, private investigators, injured and third parties; providing risk-prevention methodologies.
+  Develops medical malpractice claims information by investigating hospital, nursing home, physician and nursing negligence claims; providing unbiased opinions; determining merits of legal or accreditation actions.
+  Provides medical records information by completing initial review and liability assessment; identifying health care providers; preparing releases and letters to obtain complete medical records, x-rays, and billings; organizing, chronologically summarizing, analyzing, and indexing medical records and billings; preparing a timeline of events; transcribing handwritten chart/record notes; preparing tables, charts, and graphs of data; recommending and preparing documents for review by the expert advisory panel.
+  Identifies potential medical personnel/witnesses by deciphering names from medical documents; locating important witnesses; interviewing witnesses; suggesting list of deponents; scheduling and summarizing depositions; reviewing and assisting attorney in preparation of preliminary and final witness lists.
+  Identifies medical experts by recommending areas of expertise to be consulted; obtaining and providing a list of potential experts; locating specifically named experts (defense and/or plaintiff); researching backgrounds of experts; obtaining curriculum vitaes; interacting with experts; preparing documents for review by experts; maintaining reference notebook on experts.
+  Completes medical research by utilizing personal knowledge, experience, reference library, and internet; defining and interpreting medical language; defining and interpreting medical procedures; defining and interpreting medical-hospital/office policies.
+  Supports case presentation by drafting interrogatories and admissions; preparing slides, spreadsheets, charts, and graphs based on investigative findings.
+  Maintains legal and accreditation compliance by adhering to policy positions concerning federal, state, and local regulations, and Joint Commission on Accreditation of Healthcare Organizations (JCAHO) standards; anticipating emerging issues.
+  Improves quality results by studying, evaluating, and re-designing processes; implementing changes.
+  Updates job knowledge by participating in educational opportunities; reading professional publications; maintaining personal networks; participating in professional organizations.
+  Enhances department and organization reputation by accepting ownership for accomplishing new and different requests; exploring opportunities to add value to job accomplishments.
+  Nurse Consultant Skills and Qualifications:
+  Nursing Skills, Health Promotion and Maintenance, Hospital Environment, Procedural Skills, Informing Others, Verbal Communication, Documentation Skills, Functional and Technical Skills, Perspective, Use of Medical Technologies, Medical Teamwork`,
+  `Occupational Therapy Assistant Job Responsibilities:
+  Helps occupational therapy clients by providing rehabilitative services, under the direction of occupational therapists, to persons with mental, physical, emotional, or developmental impairments.
+
+
+  Occupational Therapy Assistant Job Duties:
+  Verifies client information by interviewing client; reviewing and/or recording medical history; confirming purpose of treatment.
+  Helps clients improve their quality of life by helping them compensate for limitations, such as, helping injured workers improve their motor skills and re-enter the labor force, and teaching persons with learning disabilities to prepare meals or to use public transportation and increase their independence,
+  Follows treatment plan developed in collaboration with occupational therapist by teaching techniques, such as, the proper method of moving from a bed into a wheelchair, and the best ways to stretch and limber muscles; monitoring activities to make sure they are performed correctly; providing encouragement, bringing treatment that is not having the intended effect to the attention of the therapist
+  Maintains patient occupational therapy records by recording client's progress for use by the occupational therapist.
+  Generates revenues by recording billing information of services rendered.
+  Maintains safe, secure, and healthy work environment by establishing, following, and enforcing standards and procedures; complying with legal regulations.
+  Keeps supplies ready by inventorying stock; placing orders; verifying receipt.
+  Keeps equipment operating by following operating instructions; troubleshooting breakdowns; maintaining supplies; performing preventive maintenance; calling for repairs.
+  Serves and protects the occupational therapy practice by adhering to professional standards, policies and procedures, federal, state, and local requirements, and Joint Commission on Accreditation of Healthcare Organizations (JCAHO) standards.
+  Updates job knowledge by participating in educational opportunities; reading professional publications; maintaining personal networks; participating in professional organizations.
+  Enhances occupational therapy practice reputation by accepting ownership for accomplishing new and different requests; exploring opportunities to add value to job accomplishments.
+  Occupational Therapy Assistant Skills and Qualifications:
+  Documentation Skills, Scheduling, Verbal Communication, Patient Services, Health Care Administration, Hospital Environment, Teamwork, Use of Medical Technologies, Bedside Manner, Dependability, Health Promotion and Maintenance`,
+  `Civil Engineer Job Responsibilities:
+  A civil engineer completes construction projects by preparing engineering design and documents; confirming specifications.
+
+  Civil Engineer Job Duties:
+  Designs construction projects by studying project concept, architectural drawings, and models.
+  Prepares engineering design by collecting and studying reports, maps, drawings, blueprints, aerial photographs and tests on soil composition, terrain, hydrological characteristics, and related topographical and geologic data.
+  Determines project costs by calculating labor, material, and related costs.
+  Prepares feasibility study by analyzing engineering design; conducting environmental impact studies; assembling data.
+  Prepares engineering documents by developing construction specifications, plans, and schedules.
+  Confirms adherence to construction specifications and safety standards by monitoring project progress; inspecting construction site; verifying calculations and placements.
+  Fulfills project requirements by training and guiding operators.
+  Maintains operations by enforcing project and operational policies and procedures.
+  Provides engineering information by answering questions and requests.
+  Complies with federal, state, and local legal requirements by studying existing and new legislation; anticipating future legislation; enforcing adherence to requirements; advising management on needed actions.
+  Maintains project data base by writing computer programs; entering data; completing backups.
+  Contributes to team effort by accomplishing related results as needed.
+  Civil Engineer Skills and Qualifications:
+  CAD, AutoCAD, Design Skills, Site Development, Database Design, Planning, Conceptual Skills, Legal Compliance, Drafting, LISP, Problem Solving`,
+  `Hair Stylist Job Responsibilities:
+  A hair stylist serves customers by preparing, conditioning, and styling hair.
+
+  Hair Stylist Job Duties:
+  Maintains supplies by checking stock; anticipating needs; placing orders; verifying receipt.
+  Prepares hair for styling by analyzing hair condition; shampooing and treating hair.
+  Conditions hair and scalp by applying treatments.
+  Plans desired effect by studying facial features; examining potential styles; conferring with customer; making recommendations.
+  Produces desired effect by arranging, shaping, curling, cutting, trimming, setting, bleaching, dyeing, and tinting hair.
+  Maintains quality service by following organization standards.
+  Maintains safe and healthy conditions by following organization standards and legal regulations.
+  Obtains revenue by recording or collecting charges.
+  Maintains technical knowledge by attending educational workshops; reviewing publications.
+  Contributes to team effort by accomplishing related results as needed. Skills/Qualifications: Listening, Supply Management, Analyzing Information , Customer Focus, Client Relationships, Organization, Creativity, Attention to Detail, Confidentiality, Professionalism, Verbal Communication
+  Hair Stylist Skills and Qualifications:
+  Listening, Supply Management, Analyzing Information , Customer Focus, Client Relationships, Organization, Creativity, Attention to Detail, Confidentiality, Professionalism, Verbal Communication`,
+  `Veterinarian Job Responsibilities:
+  A veterinarian maintains animal health by diagnosing and treating diseases and injuries.
+
+  Veterinarian Job Duties:
+  Determines nature of disease or injury by examining pets; examining large animals.
+  Restores health by treating animals surgically or medically.
+  Prevents rabies, burcellosis, and other disorders by testing animals; providing inoculations.
+  Promotes health of animals by advising animal owners about sanitation measures, feeding, and general care.
+  Documents actions by completing forms, reports, logs, and animal records; reporting diseases to public health officials.
+  Updates job knowledge by participating in educational opportunities; reading professional publications; maintaining personal networks; participating in professional organizations.
+  Enhances ventinary clinic reputation by accepting ownership for accomplishing new and different requests; exploring opportunities to add value to job accomplishments. Skills/Qualifications: Knowledge of Animal Behavior, Animal Husbandry, Basic Safety, Documentation Skills, Customer Service, Performing Diagnostic Procedures, Use of Medical Technologies, Job Knowledge, Judgment, Integrity, Verbal Communication
+  Veterinarian Skills and Qualifications:
+  Knowledge of Animal Behavior, Animal Husbandry, Basic Safety, Documentation Skills, Customer Service, Performing Diagnostic Procedures, Use of Medical Technologies, Job Knowledge, Judgment, Integrity, Verbal Communication`,
+]
