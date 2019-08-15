757b894dc9757d45f7fa6662132b81487ea6dc60
Ben Hall <benji2357@gmail.com>
2019-08-15 21:29:49 +0100

    feat(home): more powerful filters

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -11,6 +11,7 @@ import {
 import gql from 'graphql-tag'
 import React from 'react'
 import { Query } from 'react-apollo'
+import { useDebounce } from 'use-debounce'
 import { searchJobs } from '../../../graphql/queries'
 import {
   SearchableJobFilterInput,
@@ -19,11 +20,22 @@ import {
 } from '../../../API'
 import JobListItem from './JobListItem'
 
+const DEBOUNCE_TIME = 300
+
 const initialSearchParams = new URLSearchParams(location.search) // eslint-disable-line no-restricted-globals
 const initialSearchDescription =
   initialSearchParams.get('searchDescription') || ''
-const initialSearchText = initialSearchParams.get('searchTitle') || ''
+const initialSearchLocation = initialSearchParams.get('searchLocation') || ''
+const initialSearchTitle = initialSearchParams.get('searchTitle') || ''
+const initialSearchType = initialSearchParams.get('searchType') || ''
 let initialRemoteValue: undefined | boolean
+
+const initialFilterValues = [
+  initialRemoteValue,
+  initialSearchDescription,
+  initialSearchLocation,
+  initialSearchType,
+]
 
 const initialRemoteString = initialSearchParams.get('remote')
 
@@ -36,38 +48,67 @@ if (initialRemoteString) {
 
 export default function Home({ navigate }: RouteComponentProps) {
   const [filtersApplied, setFiltersApplied] = React.useState(
-    initialRemoteValue !== undefined,
+    initialFilterValues.some(Boolean),
   )
   const [remoteValue, setRemoteValue] = React.useState(initialRemoteValue)
   const [searchDescription, setSearchDescription] = React.useState(
     initialSearchDescription,
   )
-  const [searchTitle, setSearchTitle] = React.useState(initialSearchText)
+  const [searchLocation, setSearchLocation] = React.useState(
+    initialSearchLocation,
+  )
+  const [searchTitle, setSearchTitle] = React.useState(initialSearchTitle)
+  const [searchType, setSearchType] = React.useState(initialSearchType)
+
+  const [debouncedSearchDescription] = useDebounce(
+    searchDescription,
+    DEBOUNCE_TIME,
+  )
+  const [debouncedSearchTitle] = useDebounce(searchTitle, DEBOUNCE_TIME)
+  const [debouncedSearchType] = useDebounce(searchType, DEBOUNCE_TIME)
+  const [debouncedSearchLocation] = useDebounce(searchLocation, DEBOUNCE_TIME)
 
   React.useEffect(() => {
     const searchParams = new URLSearchParams()
-    if (filtersApplied && remoteValue !== undefined)
-      searchParams.set('remote', String(remoteValue))
-    if (searchDescription)
-      searchParams.set('searchDescription', searchDescription)
-    if (searchTitle) searchParams.set('searchTitle', searchTitle)
+    if (filtersApplied) {
+      if (remoteValue !== undefined)
+        searchParams.set('remote', String(remoteValue))
+      if (debouncedSearchDescription)
+        searchParams.set(
+          'debouncedSearchDescription',
+          debouncedSearchDescription,
+        )
+      if (debouncedSearchLocation)
+        searchParams.set('debouncedSearchLocation', debouncedSearchLocation)
+      if (debouncedSearchType)
+        searchParams.set('debouncedSearchType', debouncedSearchType)
+    }
+    if (debouncedSearchTitle)
+      searchParams.set('debouncedSearchTitle', debouncedSearchTitle)
     ;(navigate as NavigateFn)(
       `/${[...searchParams].length ? '?' + searchParams : ''}`,
-      {
-        replace: true,
-      },
+      { replace: true },
     )
-  }, [filtersApplied, remoteValue, searchDescription, searchTitle]) // eslint-disable-line react-hooks/exhaustive-deps
+  } /* eslint-disable react-hooks/exhaustive-deps */, [
+    debouncedSearchDescription,
+    debouncedSearchLocation,
+    debouncedSearchTitle,
+    debouncedSearchType,
+    filtersApplied,
+    remoteValue,
+  ] /* eslint-enable react-hooks/exhaustive-deps */)
 
   let filter: SearchableJobFilterInput = {}
 
-  if (filtersApplied && remoteValue !== undefined)
-    filter.remote = { eq: remoteValue }
-  if (searchDescription)
-    filter.description = {
-      matchPhrase: searchDescription,
-    }
-  if (searchTitle) filter.title = { match: searchTitle }
+  if (filtersApplied) {
+    if (remoteValue !== undefined) filter.remote = { eq: remoteValue }
+    if (debouncedSearchLocation)
+      filter.location = { match: debouncedSearchLocation }
+    if (debouncedSearchDescription)
+      filter.description = { matchPhrase: debouncedSearchDescription }
+    if (debouncedSearchType) filter.type = { match: debouncedSearchType }
+  }
+  if (debouncedSearchTitle) filter.title = { match: debouncedSearchTitle }
 
   const variables = Object.keys(filter).length ? { filter } : undefined
 
@@ -80,12 +121,6 @@ export default function Home({ navigate }: RouteComponentProps) {
           onChange={e => setSearchTitle(e.target.value)}
           value={searchTitle}
         />
-        <TextField
-          label="Job description"
-          supportiveText="Search for a phrase in the job description"
-          onChange={e => setSearchDescription(e.target.value)}
-          value={searchDescription}
-        />
       </Paper>
       <Paper>
         <Toggle
@@ -94,29 +129,47 @@ export default function Home({ navigate }: RouteComponentProps) {
           onChange={() => setFiltersApplied(!filtersApplied)}
         />
         {filtersApplied && (
-          <RadioButtonGroup label="Remote">
-            <RadioButton
-              checked={remoteValue === undefined}
-              name="remote"
-              onChange={() => setRemoteValue(undefined)}
-            >
-              Off
-            </RadioButton>
-            <RadioButton
-              checked={remoteValue === true}
-              name="remote"
-              onChange={() => setRemoteValue(true)}
-            >
-              Remote
-            </RadioButton>
-            <RadioButton
-              checked={remoteValue === false}
-              name="remote"
-              onChange={() => setRemoteValue(false)}
-            >
-              Not remote
-            </RadioButton>
-          </RadioButtonGroup>
+          <>
+            <TextField
+              label="Job description"
+              supportiveText="Search for a phrase in the job description"
+              onChange={e => setSearchDescription(e.target.value)}
+              value={searchDescription}
+            />
+            <TextField
+              label="Type"
+              onChange={e => setSearchType(e.target.value)}
+              value={searchType}
+            />
+            <TextField
+              label="Location"
+              onChange={e => setSearchLocation(e.target.value)}
+              value={searchLocation}
+            />
+            <RadioButtonGroup label="Remote">
+              <RadioButton
+                checked={remoteValue === undefined}
+                name="remote"
+                onChange={() => setRemoteValue(undefined)}
+              >
+                Off
+              </RadioButton>
+              <RadioButton
+                checked={remoteValue === true}
+                name="remote"
+                onChange={() => setRemoteValue(true)}
+              >
+                Remote
+              </RadioButton>
+              <RadioButton
+                checked={remoteValue === false}
+                name="remote"
+                onChange={() => setRemoteValue(false)}
+              >
+                Not remote
+              </RadioButton>
+            </RadioButtonGroup>
+          </>
         )}
       </Paper>
       <Query<SearchJobsQuery, SearchJobsQueryVariables>
