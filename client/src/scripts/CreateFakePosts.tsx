4a340ede23bd3ea39f13a9e876d765f2cd09d0b1
Ben Hall <benji2357@gmail.com>
2019-09-16 18:25:02 +0100

    chore(scripts): fix CreateFakePosts

--- client/src/scripts/CreateFakePosts.tsx
+++ client/src/scripts/CreateFakePosts.tsx
@@ -3,36 +3,45 @@ import * as faker from 'faker'
 import * as React from 'react'
 import { useMutation } from '@apollo/react-hooks'
 import { CREATE_POST } from '../components/pages/CreatePost'
+import { useAppState } from '../components/AppStateContainer'
 ;(faker as any).locale = 'en_GB'
 
 const integerBetween = (min: number, max: number): number =>
   Math.floor((max - min) * Math.random() + min)
 
-const generatePost = (): {
+const generatePost = (
+  userId: string,
+): {
   body: string
   location: string
   tags: string[]
   title: string
+  userId: string
 } => ({
   body: faker.lorem.paragraphs(),
   location: faker.fake('{{address.city}}, {{address.county}}'),
   tags: [...Array(integerBetween(0, 5)).keys()].map(() => faker.lorem.word()),
   title: faker.lorem.words(),
+  userId,
 })
 
-const generateFakePosts = (n: number) => {
+const generateFakePosts = (n: number, userId: string) => {
   const posts = []
-  for (let i = 0; i < n; i++) posts.push(generatePost())
+  for (let i = 0; i < n; i++) posts.push(generatePost(userId))
   return posts
 }
 
 export default function CreateFakePosts({ n }: { n: number }) {
   const [create] = useMutation(CREATE_POST)
-
+  const [{ loading, user }] = useAppState()
+  if (loading) return null
+  if (!user) throw Error('Must sign in')
   return (
     <Button
       onClick={() =>
-        generateFakePosts(n).forEach(input => create({ variables: { input } }))
+        generateFakePosts(n, user.id).forEach(input =>
+          create({ variables: { input } }),
+        )
       }
     >
       Click to create {n} fake post{n === 1 ? '' : 's'}
